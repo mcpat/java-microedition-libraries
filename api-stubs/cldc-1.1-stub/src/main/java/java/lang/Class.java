2e3b9ec87c7c381aebb94f0e117234fee7d57edf
Vladimir Schoener <schoener@wapl.de>
2010-11-12 14:51:40 +0100

    Class added

---
+++ api-stubs/cldc-1.1-stub/src/main/java/java/lang/Class.java
@@ -0,0 +1,62 @@
+package java.lang;
+/**
+ * @since JDK1.0, CLDC 1.0 
+ */
+
+import java.io.InputStream;
+
+public class Class {
+
+	/**
+	 * @throws ClassNotFoundException
+	 * @throws Error
+	 */
+	public static Class forName(String className) throws ClassNotFoundException {
+		return null;
+	}
+	
+	public String getName() {
+		return null;
+		
+	}
+	
+	public InputStream getResourceAsStream(String name) {
+		return null;
+		
+	}
+	
+	public boolean isArray() {
+		return false;
+		
+	}
+	
+	/**
+	 * @throws NullPointerException
+	 */
+	public boolean isAssignableFrom(Class cls) {
+		return false;
+		
+	}
+	
+	public boolean isInstance(Object obj) {
+		return false;
+	}
+
+	public boolean isInterface() {
+		return false;
+		
+	}
+
+	/**
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
+	public Object newInstance() throws InstantiationException, IllegalAccessException {
+		return null;
+	}
+	
+	public String toString() {
+		return null;
+		
+	}
+}
